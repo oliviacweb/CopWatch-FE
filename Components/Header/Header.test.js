dd24e894a4eaf7f20f79f0d4632eee8917fd2030
Dave Pernitz <pernitz@uwalumni.com>
2020-07-20 13:49:33 -0600

    Fix travis.yml file
    Update travis.yml
    
    Update travis.yml
    
    Update package.json
    
    Update travis.yml
    
    Update dependencies
    
    Update travis.yml

--- Components/Header/Header.test.js
+++ Components/Header/Header.test.js
@@ -4,7 +4,7 @@ import Header from './Header'
 import { render, fireEvent, waitFor } from "react-native-testing-library"
 
 describe('Header', () => {
-    xit('Should display CopWatch', async () => {
+    it('Should display CopWatch', async () => {
         const { getByText } = render(<Header />)
 
         const title = await waitFor(() => getByText("CopWatch"))
