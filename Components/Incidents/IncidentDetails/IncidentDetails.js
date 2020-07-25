0535acdea106314a0d05dd40d9410c2a0eaa3a32
Karl Nielsen <karl.g.nielsen@gmail.com>
2020-07-25 17:04:26 -0600

    :art: Styling IncidentDetails Component

--- Components/Incidents/IncidentDetails/IncidentDetails.js
+++ Components/Incidents/IncidentDetails/IncidentDetails.js
@@ -1,21 +1,39 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
+import Footer from '.'
 
 export default function IncidentDetails(props) {
     const incident = props.route.params.incident
 
     return (
         <View>
-            <Text>Incident Id: {incident.id}</Text>
-            <Text>Date: {incident.created_date}</Text>
-            <Text>City: {incident.city}</Text>
-            <Text>State: {incident.state}</Text>
-            <Text>Zip: {incident.zip_code}</Text>
-            <Text>Parties: {incident.parties}</Text>
-            <Text>Description: {incident.description}</Text>
-            <Text>Officer: {incident.officer_name}</Text>
-            <Text>Badge Number: {incident.badge_number}</Text>
+            <View style={styles.container}>
+                <Text style={styles.text}>Incident Id: {incident.id}</Text>
+                <Text style={styles.text}>Date: {incident.created_date}</Text>
+                <Text style={styles.text}>City: {incident.city}</Text>
+                <Text style={styles.text}>State: {incident.state}</Text>
+                <Text style={styles.text}>Zip: {incident.zip_code}</Text>
+                <Text style={styles.text}>Parties: {incident.parties}</Text>
+                <Text style={styles.text}>Description: {incident.description}</Text>
+                <Text style={styles.text}>Officer: {incident.officer_name}</Text>
+                <Text style={styles.text}>Badge Number: {incident.badge_number}</Text>
+            </View>
+            <Footer navigation={navigation}>
         </View>
     )
     
 }
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: "#003366",
+        color: '#fff',
+        padding: 10,
+        height: '100%',
+
+    },
+    text: {
+        color: '#fff',
+        fontSize: 20,
+    }
+})
