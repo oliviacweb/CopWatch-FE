af1e355d0ca063ecbfc798405ddb2ecc518e1a06
Dave Pernitz <pernitz@uwalumni.com>
2020-07-25 10:33:15 -0600

    Update apiCalls with deployed address

--- apiCalls.js
+++ apiCalls.js
@@ -1,7 +1,5 @@
-// const axios = require('axios');
-
 export const fetchIncidents = async () => {
-    const url = `http://localhost:5000/api/v1/reports`
+    const url = 'https://copwatch-be.herokuapp.com/api/v1/reports'
     const data = await fetch(url,{
         method: "GET"
     })
@@ -12,26 +10,8 @@ export const fetchIncidents = async () => {
     return data
 }
 
-// export const fetchIncidents = async () => {
-//     const url = `http://localhost:5000/api/v1/reports`
-//     try {
-//         const response = await fetch(url);
-//             if(!response.ok) {
-//                 console.log(response)
-//                 throw new error(error);
-//             }
-//         const data = await response.json();
-//         console.log(data)
-//         return data;
-//     }
-//     catch (error) {
-//         return false
-//     }
-// }
-
-
 export const postIncident = async (report) => {
-    const url = `http://localhost:5000/api/v1/reports/new`
+    const url = 'https://copwatch-be.herokuapp.com/api/v1/reports/new'
     fetch(url, {
         method: 'POST',
         headers: {
@@ -53,62 +33,3 @@ export const postIncident = async (report) => {
         .then(response => response.json())
         .catch(err => console.error(err))
 }
-
-
-
-// export const fetchIncidents = async () => {
-//     const corsAnywhere = `https://cors-anywhere.herokuapp.com/`
-//     const url = `${corsAnywhere}http://localhost:5000/api/v1/reports`
-//     const data = await fetch(url)
-//     return data
-// }
-// const corsAnywhere = `https://cors-anywhere.herokuapp.com/`
-// const url = `${corsAnywhere}http://localhost:5000/api/v1/reports`
-// const data = await fetch(url)
-// return data
-
-
-// export const fetchIncidents = async () => {
-//     return await fetch('https://fe-cors-proxy.herokuapp.com', {
-//         method: "GET",
-//         headers: {
-//             "Target-URL": "http://localhost:5000/api/v1/reports"
-//         }
-//     })
-//     .then(response => response.json())
-//     // .then(foxData => console.log(foxData))
-//     .catch(error => console.error(error))
-// }
-
-
-// export const fetchIncidents = () => {
-//     console.log('fetch! ')
-//     const corsAnywhere = `https://cors-anywhere.herokuapp.com/`
-//     const url = `${corsAnywhere}http://localhost:5000/api/v1/reports`
-
-//         return axios.get(url)
-//         // {
-//         // //   headers: {
-//         // //     'Content-Type': 'multipart/form-data'
-//         // //   }
-//         // })
-
-// }
-// export const fetchIncidents = () => {
-//     const corsAnywhere = `https://cors-anywhere.herokuapp.com/`
-//     const url = `${corsAnywhere}http://localhost:5000/api/v1/reports`
-//     axios.get(url)
-//     .then(function (response) {
-//       // handle success
-//       console.log(response);
-//       return response
-//     })
-//     .catch(function (error) {
-//       // handle error
-//       console.log(error);
-//     })
-//     .finally(function () {
-//       // always executed
-//     });
-
-// }
