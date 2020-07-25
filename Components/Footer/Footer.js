c3660f8a46cbfb30a7005a00178854b8a4faf94e
Karl Nielsen <karl.g.nielsen@gmail.com>
2020-07-25 13:54:07 -0600

    :art: Styling footer nav icons; Add Twitter icon

--- Components/Footer/Footer.js
+++ Components/Footer/Footer.js
@@ -5,14 +5,14 @@ import { NavigationContainer } from '@react-navigation/native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { TouchableHighlight } from 'react-native-gesture-handler';
 
-const homeIcon = <Icon name="home" size={60} color="#003366" />;
-const addReportIcon = <Icon name="edit" size={60} color="#003366" />;
-const viewIncidentsIcon = <Icon name="list-alt" size={60} color="#003366" />;
-const twitterIcon = <Icon name="twitter" size={60} color="#003366" />;
 
 // const Stack = createStackNavigator();
 
 export default function Footer({ navigation }) {
+    const homeIcon = <Icon name="home" size={60} color="#003366" />;
+    const addReportIcon = <Icon style={styles.iconFileReport} name="edit" size={60} color="#003366" />;
+    const viewIncidentsIcon = <Icon style={styles.iconViewReports} name="list-alt" size={60} color="#003366" />;
+    const twitterIcon = <Icon name="twitter" size={60} color="#003366" />;
     const handleIncidentsView = () => {
         navigation.navigate("Incidents Container")
     }
@@ -25,22 +25,31 @@ export default function Footer({ navigation }) {
 
     return (
       <View style={styles.footer}>
-        <TouchableHighlight onPress={() => handleHomeView()}>
-            <View style={styles.button}>
-                {homeIcon}
-                <Text>Home</Text>
-            </View>
+        <TouchableHighlight
+          style={styles.navItem}
+          onPress={() => handleHomeView()}
+        >
+          <View style={styles.button}>
+            {homeIcon}
+            <Text style={styles.iconTitle}>Home</Text>
+          </View>
         </TouchableHighlight>
         <TouchableHighlight onPress={() => handleFileReportView()}>
           <View style={styles.button}>
             {addReportIcon}
-            <Text>File A Report</Text>
+            <Text style={styles.iconTitle}>File A Report</Text>
           </View>
         </TouchableHighlight>
         <TouchableHighlight onPress={() => handleIncidentsView()}>
           <View style={styles.button}>
             {viewIncidentsIcon}
-            <Text>View Incidents</Text>
+            <Text style={styles.iconTitle}>View Incidents</Text>
+          </View>
+        </TouchableHighlight>
+        <TouchableHighlight onPress={() => handleIncidentsView()}>
+          <View style={styles.button}>
+            {twitterIcon}
+            <Text style={styles.iconTitle}>Twitter</Text>
           </View>
         </TouchableHighlight>
       </View>
@@ -53,24 +62,29 @@ const styles = StyleSheet.create({
         bottom: 0,
         width: '100%',
         height: '15%',
-        // display: 'flex',
         backgroundColor: '#fff',
         flex: 1,
         flexDirection: 'row',
-        justifyContent: 'space-around'
-    },
-    button: {
-        // backgroundColor: 'red',
-        width: '100%',
-    },
-    icon: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center"  
-    },
-    iconTitle: {
-        flex: 1,
-        justifyContent: "center",
+        justifyContent: 'space-around',
         alignItems: "center"
     },
+    button: {
+        width: '100%',
+        textAlign: "center"
+    },
+    iconFileReport: {
+        marginLeft: 20
+    },
+    iconViewReports: {
+        marginLeft: 15
+    },
+    iconTitle: {
+        textAlign: "center"
+    },
+    navItem: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column"
+    }
 })
