75659e28e4429b146d165123671bc0067a9febce
Dave Pernitz <pernitz@uwalumni.com>
2020-07-27 12:42:43 -0600

    Fix bug with location

--- Components/Incidents/IncidentsContainer/IncidentsContainer.js
+++ Components/Incidents/IncidentsContainer/IncidentsContainer.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react'
-import { ScrollView, TextInput, StyleSheet, Button, View, Text } from 'react-native'
+import { ScrollView, TextInput, StyleSheet, Button, View, Text, Platform } from 'react-native'
 import { fetchIncidents } from '../../../apiCalls'
 import IncidentCard from '../IncidentCard/IncidentCard'
+import Footer from '../../Footer/Footer'
+
 
 export default function IncidentsContainer({ navigation }) {
     const [ incidents, updateIncidents ] = useState([])
